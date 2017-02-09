dde25e4bfd29bf55952af756fdf41c1ea4c00168
qwerty258 <zyf26256@gmail.com>
2017-02-09 17:39:03 +0800

    update test source code path

--- unvd_hc/compile.sh
+++ unvd_hc/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcc -std=c99 -ggdb -I '../include' -fpic -c unvd_hc.h unvd_hc.c
 gcc -shared -o libunvd_hc.so unvd_hc.o -lhcnetsdk -lhpr -lHCCore
-gcc -std=c99 -ggdb -I '../include' -I './' -L'./' unvd_hc.h ../test.c -lunvd_hc
+gcc -std=c99 -ggdb -I '../include' -I './' -L'./' unvd_hc.h ../tests/test_hc.c -lunvd_hc
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:'./'
